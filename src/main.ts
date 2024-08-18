0021433c81addf9ae82a69d4fdcfc1c5fed7e348
Felipe Jung <felipemattoseu@gmail.com>
2024-08-18 14:39:17 -0400

    Add history and fidelity card sections

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import "./styles/global.css";
 
 import "./styles/header.css";
 import "./styles/input.css";
+import "./styles/client.css";
+import "./styles/history.css";
+import "./styles/fidelity-card.css";
+import "./styles/cuts-left.css";
+
 import "./styles/main.css";
